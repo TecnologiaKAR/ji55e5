83924749df211f208cd052f67e5118423d3245a6
TecnologiaKAR <90747580+TecnologiaKAR@users.noreply.github.com>
2023-05-17 22:40:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ji55e5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ji55e5)
---
+++ src/app/demo/tree-table-dynamic-columns-demo.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { TreeNode } from 'primeng/api';
+import { NodeService } from '../../service/nodeservice';
+
+@Component({
+    selector: 'tree-table-dynamic-columns-demo',
+    templateUrl: './tree-table-dynamic-columns-demo.html'
+})
+export class TreeTableDynamicColumnsDemo implements OnInit {
+    files: TreeNode[];
+
+    cols: any[];
+
+    constructor(private nodeService: NodeService) {}
+
+    ngOnInit() {
+        this.nodeService.getFilesystem().then((files) => (this.files = files));
+        this.cols = [
+            { field: 'name', header: 'Name' },
+            { field: 'size', header: 'Size' },
+            { field: 'type', header: 'Type' }
+        ];
+    }
+}
